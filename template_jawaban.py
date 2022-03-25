67f74b68b21939649e94456b6f847cb6c498feeb
AdiSangjaya <adi.sangjaya@gmail.com>
2022-03-25 20:17:11 +0700

    tadi ada yang salah2

--- template_jawaban.py
+++ template_jawaban.py
@@ -1,7 +1,6 @@
 # 6A/19090019/Adi Sangjaya
 # 6A/19090121/Moh Farid Nurul Ikhsani
 
-
 import os
 from flask import Flask,jsonify,request
 from flask_httpauth import HTTPTokenAuth
@@ -13,20 +12,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "user.db"))
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
 auth = HTTPTokenAuth(scheme='Bearer')
-
-
 class User(db.Model):
     username = db.Column(db.String(80), unique=True,nullable=False, primary_key=True)
     password = db.Column(db.String(80), unique=False,nullable=False, primary_key=False)
     token = db.Column(db.String(225), unique=True,nullable=True, primary_key=False)
-
-
-
 
 @app.route("/api/v1/login", methods=["POST"])
 def login():
@@ -44,18 +37,14 @@ def login():
         "data": access_token,
     })
 
- 
 @auth.verify_token
 def verify_token(token):
   username=User.query.filter_by(token=token).first()
   return username.username
 
-
 @app.route("/api/v2/users/info", methods=["POST"])
 @auth.login_required
 def info():
   return "Hello, {}!".format(auth.current_user())
-
 if __name__ == '__main__':
     app.run(debug = True, port= 4000)
-  
